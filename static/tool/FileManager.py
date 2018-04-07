9a2c68cbff8120e3b2d50b7c3666c7ef6e049095
SerhiiRi <sergiy19981@gmail.com>
2018-04-07 20:11:08 +0200

    Change IOrm

---
+++ static/tool/FileManager.py
@@ -0,0 +1,6 @@
+# Tu musi być klasa lub do chuja dużo procedur
+# z możliwościa wykorzystywania ich dla szybkiego
+# tworznia katalogów, linków na objekt, kopijowania,
+# wycinania, sprawdzenia, testowania plików w
+# drzewie systemu plikowego
+# Do każdej metody musi byc stworzony Log.
