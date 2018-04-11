d9f6f7850c2f305805ea73ff895b156f6cb3cf83
Aleks <johnhenk1122@gmail.com>
2018-04-11 21:50:59 +0200

    Nowe strony
    Uzupełniłem strony brakujące, do których były gotowe przyciski. Należy jeszcze poprawić ich zawartość.

--- app.py
+++ app.py
@@ -13,6 +13,17 @@ def index():
     userData = [23487623874623764, "/static/pic/testpic.jpg"]
     return render_template('index.html', info=info, data=userData)
 
+@app.route('/info')
+def info():
+    return render_template('info.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/kontakt')
+def kontakt():
+    return render_template('kontakt.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
