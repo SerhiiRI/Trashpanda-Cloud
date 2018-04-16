5de1230971455b2239f96710d939f82081b761b9
Aleks <johnhenk1122@gmail.com>
2018-04-16 19:46:54 +0200

    Usunięto bug z firefoxem
    Problem => flex-direction: column;

--- app.py
+++ app.py
@@ -18,5 +18,9 @@ def about():
 def kontakt():
     return render_template('kontakt.html')
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html')
+
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5000)
