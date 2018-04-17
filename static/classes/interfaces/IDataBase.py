a5bc12e3196241dc1e3fe6efdebced607975fd4d
Serhii Riznychuk <30987869+SerhiiRI@users.noreply.github.com>
2018-04-17 09:00:52 +0200

    Create IDataBase.py

---
+++ static/classes/interfaces/IDataBase.py
@@ -0,0 +1,11 @@
+# Import dataBase
+
+class IDataBase(object):
+    
+    # mysql manager
+    __manager = None
+    
+    def __init__(self):
+        raise NotImplementedError("Not implement")
+        
+    
