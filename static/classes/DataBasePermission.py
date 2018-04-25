acc833421f64bbd66dfcd25d978b6e90ff759474
SerhiiRi <sergiy19981@gmail.com>
2018-04-26 01:50:16 +0200

    Create DataBasePermission(IPermission)

---
+++ static/classes/DataBasePermission.py
@@ -0,0 +1,18 @@
+# IPermission interface used to implement typical for permission needed
+from static.classes.interfaces.IPermission import IPermission
+# import module to manipulating on environments variable
+import os
+
+
+class DataBasePermission(IPermission):
+
+    def test(self):
+        if(os.environ.)
+
+    def rootPermision(self):
+
+        self.rootPermision()
+
+    def login(self):
+        self.login()
+
