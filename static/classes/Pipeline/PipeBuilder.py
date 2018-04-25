2c7f1c2cc6e6af613b1b518763c82a4c8809eb7d
SerhiiRi <sergiy19981@gmail.com>
2018-04-25 20:37:10 +0200

    Update PipeBuilder

--- static/classes/Pipeline/PipeBuilder.py
+++ static/classes/Pipeline/PipeBuilder.py
@@ -1,21 +1,39 @@
 #!/usr/bin/python3
 import threading, MySQLdb, pickle, random
-from flask_sqlalchemy import SQLAlchemy
-from static.classes.interfaces.IRunnable import IRunnable
 from multiprocessing import Queue
 from .Process import Process
-
+from .Controller import Controller
 
 class PipeBuilder(object):
-    """ the pipeline"""
+    """
+                PipeBuilder
+    """
 
     __queue = Queue()
-    __table = 0
+    __table = None
 
-    def __init__(self, function, controllers:list):
+    def __init__(self, function, controllers: Controller ):
+        """
+                      __init__
+          __Konstruktor__ klasy PipeBuilder
+        --------------------------------------
+         Twożenia polączenia z bazą danych,or-
+        az twożenia konektora na podstawie po-
+        lączneia, i przypisywania do poszczeg-
+        ólnych parametrów klasy węwnętrzenej
+         Na koncu twoży tabele w BD o nazwie
+        funkcji.
+
+        :param function: funkcja dla definiowania Objektu typu Process
+        :param controllers: objekt typu Controller, slużący dla odpalemnia go w Contenerze.
+        TODO: jeden(Obj) -> wielu(tuple(Obj,...)))
+
+        @Serhii Riznychuk
+        """
 
         self.controllers = controllers
         self.function = function
+        self.__table = function.__name__
         host = 'trashpanda.pwsz.nysa.pl'
         login = 'sergiy1998'
         passwd = 'hspybxeR98>'
@@ -26,27 +44,61 @@ class PipeBuilder(object):
         self._createQueueTable()
 
     def _createQueueTable(self):
+        """
+        private: CreateQueueTable
+        ---------------------------
+        Twożenia tabeli jeżeli tak-
+        iej nie istnieje dla podan-
+        ej nazwy, przychowywaniej w
+        zmiennej self.__table
+
+        :return: True
+        @Serhii Riznychuk
+        """
         sql = "CREATE TABLE IF NOT EXISTS %s (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, obj TEXT NOT NULL)"
         __cursor = self.__connector.cursor()
         __cursor.execute(sql, tuple(self.__table))
         self.__connector.commit()
+        return True
 
     def _deleteTable(self):
+        """
+        private: DeleteTable
+        ----------------------------
+        Zniszczenia tabeli o wlasnej
+        nazwie self.__table
+
+        :return: Boolean
+
+        @Serhii Rinzychuk
+        """
         sql = "DROP TABLE %s"
         __cursor = self.__connector.cursor()
         __cursor.execute(sql, tuple(self.__table))
         self.__connector.commit()
-        return __cursor.fetchall()
+        return True
 
     def _addObject(self):
+        """
+                    Add Object
+        ----------------------------------
+        Twożnia objektu typu class process,
+        oraz seralizwania go za pomocą mo-
+        dula pickle i polecenia dump(obj)
+        dopisywania do bazy danych
+
+        @Serhii Riznychuk
+        """
         tempProcessObject = Process(self.function, tuple(10, ), self.controllers)
+        # pickle.dump - serializacja objektu
         stringProcessObject = pickle.dump(tempProcessObject)
-        sql = "INSERT INTO `banns`(`id`, `object`) VALUES (NULL, %s)"
+        sql = "INSERT INTO `"+self.__table+"`(`id`, `object`) VALUES (NULL, %s)"
         __cursor = self.__connector.cursor()
         __cursor.execute(sql, (stringProcessObject))
+        del(tempProcessObject)
+        del(stringProcessObject)
         self.__connector.commit()
         __cursor.close()
-
 
 
     def buildTasks(self):
