f0920120fb507fcd8539ffbbe3c680eb406306ec
Aleks <johnhenk1122@gmail.com>
2018-04-28 01:34:51 +0200

    Powrót do stabilnej wersji wylogowania

--- static/scripts/js/index.js
+++ static/scripts/js/index.js
@@ -32,19 +32,19 @@ function onSignIn(googleUser) {
     closeLoginForm();
     afterlogin();
     loginButton();
-};
+}
 
 /**
  * Google wylogowania
  * */
-    function signOut() {
-        var auth2 = gapi.auth2.getAuthInstance();
--           auth2.signOut().then(function () {
--           console.log('User signed out.');
--    });
--    goTo('/')
-        //window.location.replace('https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue=http://trashpanda.pwsz.nysa.pl');
-    }
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+    });
+    goTo('/')
+    //window.location.replace('https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue=http://trashpanda.pwsz.nysa.pl');
+}
 
 
 /**
