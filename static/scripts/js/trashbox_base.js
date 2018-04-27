aa1c17192fb9a3983515acbb0337de5d2b8199a9
Aleks <johnhenk1122@gmail.com>
2018-04-28 00:23:52 +0200

    Templates implement
    Przerzuciłem część kodu do dekoratorów szablonu

--- static/scripts/js/trashbox_base.js
+++ static/scripts/js/trashbox_base.js
@@ -1,24 +1,39 @@
-    // Skrypt animacji wysuwania paska nav i footer
-    var menu = {'nav' : false, 'foo' : false};
-    //console.log(menu['nav']);
-    function fooAct(what, where) {
-        if(menu[what] == false) {
-            if(what == 'nav') {
+// Skrypt animacji wysuwania paska nav i footer
+var menu = {'nav': false, 'foo': false};
+
+//console.log(menu['nav']);
+function fooAct(what, where) {
+    if (menu[what] == false) {
+        if (what == 'nav') {
             document.getElementById(what).style = where + ": 0px";
+            document.getElementById(what).style.height = "70px";
+            document.getElementById('nav-inside').style.display = 'initial';
+            document.getElementById('nav-inside').style.opacity = '1';
             menu[what] = true;
-            }else if(what == 'foo') {
-                document.getElementById(what).style = where + ": 0px";
-                document.getElementById(what).style.height = "40px";
-                menu[what] = true;
-            }
-        }else{
-            if(what == 'nav') {
-                document.getElementById(what).style = where + ": -45px";
-                menu[what] = false;
-            }else if(what == 'foo'){
-                document.getElementById(what).style = where + ": -45px";
-                document.getElementById(what).style.height = "60px";
-                menu[what] = false;
-            }
+        } else if (what == 'foo') {
+            document.getElementById(what).style = where + ": 0px";
+            document.getElementById(what).style.height = "40px";
+            document.getElementById('foo-inside').style.display = 'initial';
+            document.getElementById('foo-inside').style.opacity = '1';
+            menu[what] = true;
+        }
+    } else {
+        if (what == 'nav') {
+            document.getElementById(what).style = where + ": -75px";
+            document.getElementById(what).style.height = "100px";
+            document.getElementById('nav-inside').style.opacity = '0';
+            setTimeout(function () {
+                document.getElementById('nav-inside').style.display = 'none';
+            }, 500);
+            menu[what] = false;
+        } else if (what == 'foo') {
+            document.getElementById(what).style = where + ": -75px";
+            document.getElementById(what).style.height = "100px";
+            document.getElementById('foo-inside').style.opacity = '0';
+            setTimeout(function () {
+                document.getElementById('foo-inside').style.display = 'none';
+            }, 500);
+            menu[what] = false;
         }
     }
+}
