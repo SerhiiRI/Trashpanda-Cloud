b2e7f5919705dd0e6bf51e845c77dc68a3625dae
SerhiiRi <sergiy19981@gmail.com>
2018-05-13 03:39:23 +0200

    Debug TableParser. Change dict to list

--- static/classes/datacontroller/TableParsers.py
+++ static/classes/datacontroller/TableParsers.py
@@ -1,36 +1,34 @@
 import re, random
 from copy import deepcopy
 
+
 class TableTypeParser(object):
 
-    def __init__(self, MetaTable : dict):
-        self.Meta_FieldList = list()
-        # Temp Talbe
-        self.__DataBaseTableField = MetaTable
-        self._CreateMeta()
-        self.Lambda_FieldList = self.__addLambdasToList()
+    def __init__(self, MetaTable : list):
+        self.__DataBaseTableField = MetaTable[1:]
+        self.Meta_FieldList = self._CreateMeta()
+        self.Lambda_FieldList = self.__addLambdasToList(self.Meta_FieldList)
 
     def _CreateMeta(self):
         temp = list()
         for column, typeof in self.__DataBaseTableField:
-            temp.append((column, *(re.split(r'[()]', typeof)[0:-1] if re.search(r'\([0-9]{1,4}\)', typeof) else [typeof, "1000"])))
-        self.__DataBaseTableField = temp
+            temp.append([column, *((re.split(r'[()]', typeof)[0:-1]))] if re.search(r'\([0-9]{1,4}\)', typeof) else [column, typeof, "100"] )
+        return temp
 
     def __lambda__radomizator(self, typ : str, limiter : int = 20, Prefix = "", StringSet : str =" qwertyuiopasdfghjklzxcbvbnm", NumberSet : str ="1234567890", SymbolSet : str ="./,_-+()"):
         String = StringSet + NumberSet + SymbolSet
         if (typ == "varchar" or typ == "text"):
-            return lambda pre="": '"{}:{}"'.format(pre if(len(pre) > 1) else Prefix,\
-                "".join([random.choice(String) for _ in \
-                         range(limiter - 1 - len(pre if(len(pre) > 1) else Prefix))]))
+            return lambda pre="": '"{}{}"'.format(pre if(len(pre) > 1) else Prefix,\
+                "".join([random.choice(String) for _ in range(int(limiter) - 1 - len(pre if(len(pre) > 1) else Prefix))]))
         elif (typ == "int"):
-            return lambda x=0 : random.choice(range(limiter))
+            return lambda x = 0 : random.choice(range(int(limiter)))
         else:
-            return lambda x=0: 0
+            return lambda x = 0 : 0
 
-
-    def __addLambdasToList(self):
-        self.Lambda_FieldList = list()
-        for meta in self.__DataBaseTableField:
+    def __addLambdasToList(self, listToChange : list):
+        temp = list()
+        for meta in listToChange:
+            print(meta)
             meta_column, meta_type, meta_type_len = deepcopy(meta)
-            self.Lambda_FieldList.append((self.__lambda__radomizator(meta_type, meta_type_len), meta_type_len, meta_column, meta_type))
-        return 0
+            temp.append((self.__lambda__radomizator(meta_type, meta_type_len), meta_type_len, meta_column, meta_type))
+        return temp
