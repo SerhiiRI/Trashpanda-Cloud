573a549e38099474b14c13f31172d1a270d9b576
Mikus <mikolaj.rychel@gmail.com>
2018-05-22 20:23:30 -0700

    Last Pusz ^^

--- app.py
+++ app.py
@@ -139,7 +139,8 @@ def mytrashbox(pathToDir):
         finalPath = '/' + session['googleID'] + finalPath + '/'
     else:
         return render_template('info_pages/404.html'), 404
-    files = FileController.gatherDiskInfo(finalPath)
+    filecontroller = FileController()
+    files = filecontroller.gatherDiskInfo(finalPath)
     print("Get pathToDir: " + pathToDir)
     print("Set currentDir: " + currentdir)
     print("Set backPath: " + backpath)
