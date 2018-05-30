c977a26f219b5f211c98b83007b24344f32db74a
SerhiiRi <sergiy19981@gmail.com>
2018-05-30 05:48:23 +0200

    remove testBD.py

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ def fortest():
 
 
 @Permission.login
-@Log(LogType.INFO, 2, "Run application", printToConsole=False)
+#@Log(LogType.INFO, 2, "Run application", printToConsole=False)
 def startServer():
     if __name__ == '__main__':
         app.run(debug=True, host="0.0.0.0", port=5000)
