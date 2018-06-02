1023178b543173d34cf700179d2ac982d10853d8
SerhiiRi <sergiy19981@gmail.com>
2018-06-02 15:58:23 +0200

    [Agent] Define empty function in FunctionList

--- static/classes/Pipeline/AvailableFunctions/FunctionList.py
+++ static/classes/Pipeline/AvailableFunctions/FunctionList.py
@@ -1,5 +1,32 @@
 def factorial(n):
+    """
     if n == 0:
         return 1
     else:
         return n * factorial(n - 1)
+    """
+    return "FACTORIAL"
+
+def bublesort(iterabe:list):
+    '''
+    :param iterable:
+    :return:
+    '''
+    return "BUBLE SORT"
+
+def searching(iterable:list, pattern):
+    '''
+
+    :param iterable:
+    :param pattern:
+    :return:
+    '''
+    return "SEARCH"
+
+functionList={
+    "factorial": factorial,
+    "bublesort": bublesort,
+    "searching": searching
+}
+
+
