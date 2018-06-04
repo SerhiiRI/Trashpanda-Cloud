38c8761175bef1ba4858aca52f4b95919826544a
SerhiiRi <sergiy19981@gmail.com>
2018-06-04 07:29:25 +0200

    [Agent] Fix bug

--- static/classes/Pipeline/AgentDocker/startup.sh
+++ static/classes/Pipeline/AgentDocker/startup.sh
@@ -4,4 +4,4 @@ git clone https://github.com/Morfeu5z/llapCloudFlask.git /server
 gcc -Wall -o /server/static/tool/Binary/cpuController /server/static/tool/Binary/Test.c
 cd /server
 python3 ./PipelineMachine.py -i 0.0.0.0 -p 5000
-/bin/basph
+/bin/bash
