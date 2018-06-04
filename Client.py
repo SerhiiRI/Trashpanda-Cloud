7f4e86b88e4faa08f8d8d63c2c1e673b724a2afd
SerhiiRi <sergiy19981@gmail.com>
2018-06-04 06:10:01 +0200

    [Agent] Update Client-File

--- Client.py
+++ Client.py
@@ -5,89 +5,82 @@
 import static.configs.EnvConf
 from static.classes.Pipeline.Agents.ClientAgent import Agent
 import argparse
+import random
+
+client = {
+    # Przy poprawnej realizacji wartość tego pola musi definiować
+    # progama automatycznie, zależąć od tego, o jakiej specyfika-
+    # cji wymagań agenta byla zrobiono. Tzn, zrozumienie proceśów
+    # wykonwczych.
+    # Np. jeżeli agent jest uzależniony węwnątrz od czasu, to wt-
+    # edy musi sie wyliczyć odpowiednią wagę wychodzącą, obojętne
+    # jaka ta waga będzie miala znaczenia.
+    # W programie te wartośći wypelnia mega-inteligentna funkcja
+    # random((range(0,101, 1)))
+    "id": random.randint(0, 10000),
+    "machine": {
+        "cpu": 16,
+        "disk": 10,
+        "ram": 10,
+    },
+    "function": {
+        "factorial": 2,
+        "bublesort": 3,
+        "searching": 4
+    },
+}
+select = {
+    "1": 80,
+    "2": 50,
+    "3": 25,
+    "4": 1
+}
+cena = 123
+"""
+import os
+os.system("clear")
+z, b = os.popen("stty size", mode="r").read().split()
+print("{:-^{x}}".format("MAIN", x=b))
+print("{:^{x}}".format("Szukamy wirtualną maczyne:", x=b))
+print("Obciążania procesora: \n(1)high\n(2)middle\n(3)low\n")
+cpu = input(">")
+client["machine"]["cpu"] = select[cpu]
+print("Potrzebujesz zużycia dysku? \n(1)high\n(2)middle\n(3)low\n(4)Operacje wylącznie liczbowe\n")
+cpu = input(">")
+client["machine"]["disk"] = select[cpu]
+print("Potrzebujesz zużycia pamięci operacyjnej?\n(1)high\n(2)middle\n(3)low\n(4)Operacje wylącznie liczbowe\n")
+cpu = input(">")
+client["machine"]["ram"] = select[cpu]
+print("Niesty nie zdążylem napisać Input dla \nfunkcyj, to poprostu określi poszczególą \nilość operacyj dla każdej z funkcji\n")
+print("(1-100) bądż grzecznym i nie\n pisz nic zpoza zakresu bo mi sie \nlieni robić na to obsluge")
+client["function"]["factorial"] = int(input("Silnia:"))
+client["function"]["bublesort"] = int(input("Sortowanie:"))
+client["function"]["searching"] = int(input("Search:"))
+print("Preferowana cena(200 zl max)")
+cena = int(input(":"))
+
+"""
+
 
 parser = argparse.ArgumentParser(description="Create agent")
-# parser.add_argument("-i", '--ip', action="store", dest="host", default='0.0.0.0', help="choose HOST-address  agent")
+parser.add_argument("-i", '--ip', action="store", dest="host", default='0.0.0.0', help="choose HOST-address  agent")
 parser.add_argument("-p", "--port", action="store", dest="ports", help="chose PORT-s to connect")
-
 if __name__ == '__main__':
     args = parser.parse_args()
     if not vars(args):
         print("Zle parametry")
     else:
         ports = args.ports.split(",")
-        client = Agent(40)
+        client = Agent(client, 40)
         for port in ports:
             client.PAGENT.append(client.hand_shacking(target_host="0.0.0.0", target_port=int(port), type_hs="order"))
-        client.hand_shacking(target_host="0.0.0.0", target_port=client.accept() ,type_hs="accept")
+        client.hand_shacking(target_host="0.0.0.0", target_port=client.accept(), type_hs="accept")
 
 """
+
 ten skrypt genteruje symulacje użytkowników 
 dla systemy wieloagentowej. dla twożenia 
 kolejek zadan na innych wirtualnych mazsyn-
 ach, pod docker-systemem
 
-"""
-
-
-"""
-class MyView(object):
-    min_columns = 100
-
-
-    def __int__(self):
-        self.lim_rows, self.lim_columns = popen("stty size", "r").read().split()
-
-    def createWindows(self, functionToDecorate,  ValueItems : list,  symbol="*"):
-        self.__maxElementInList(ValueItems)
-        def function():
-            header = "".join((symbol for x in range(self.columns)))
-            header = fg.cyan + header + cd.reset
-            functionToDecorate()
-            footer = "".join((symbol for x in range(self.columns)))
-            footer = fg.cyan + header + cd.reset
-        return function()
-
-    def decorateList(self, functionToDecorate, items):
-        nwlist = list()
-        nwlist = map(functionToDecorate, items)
-        return nwlist
-
-    def __control_col(self, string, symbol="*" ):
-        str = symbol+string
-        while (len(str)+1 < self.lim_columns):
-            str = str+" "
-        str += symbol
-        return str
-
-"""
-
-"""
-import npyscreen
-class TestApp(npyscreen.NPSApp):
-    def main(self):
-        # These lines create the form and populate it with widgets.
-        # A fairly complex screen in only 8 or so lines of code - a line for each control.
-        F  = npyscreen.Form(name = "Welcome to Npyscreen",)
-        t  = F.add(npyscreen.TitleText, name = "Text:",)
-        fn = F.add(npyscreen.TitleFilename, name = "Filename:")
-        fn2 = F.add(npyscreen.TitleFilenameCombo, name="Filename2:")
-        dt = F.add(npyscreen.TitleDateCombo, name = "Date:")
-        s  = F.add(npyscreen.TitleSlider, out_of=12, name = "Slider")
-        ml = F.add(npyscreen.MultiLineEdit,
-               value = 'try typing here!\nMutiline text, press ^R to reformat.\n'
-               max_height=5, rely=9)
-        ms = F.add(npyscreen.TitleSelectOne, max_height=4, value = [1,], name="Pick One",
-                values = ["Option1","Option2","Option3"], scroll_exit=True)
-        ms2= F.add(npyscreen.TitleMultiSelect, max_height =-2, value = [1,], name="Pick Several",
-                values = ["Option1","Option2","Option3"], scroll_exit=True)
-
-        # This lets the user interact with the Form.
-        F.edit()
-
-        print(ms.get_selected_objects())
-
-if __name__ == "__main__":
-    App = TestApp()
-    App.run()
 """
