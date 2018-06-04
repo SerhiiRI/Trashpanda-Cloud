bf53a8ffc0f317149fc02ae90c78286ba0ed0261
SerhiiRi <sergiy19981@gmail.com>
2018-06-04 06:06:50 +0200

    [Agent] Fix bug in Agents

--- static/classes/Pipeline/Agents/ClientAgent.py
+++ static/classes/Pipeline/Agents/ClientAgent.py
@@ -1,14 +1,3 @@
-from static.classes.Pipeline.dictionary.agent import agent as clientSet
-from static.classes.Pipeline.dictionary.ValueConfigurations import constant_values as comparator
-from static.classes.Pipeline.PipeBuilder import PipeBuilder
-from static.classes.Pipeline.Controller import ControllerServer
-import time
-import argparse
-from static.classes.Pipeline.AvailableFunctions.FunctionList import factorial
-from static.classes.Pipeline.Agents.PipeAgent import Agent
-from static.classes.Pipeline.dictionary.agent import agent
-from multiprocessing import Queue
-from threading import current_thread
 import socket
 import random
 import pickle
@@ -16,8 +5,7 @@ import codecs
 
 
 class Agent:
-
-    def __init__(self, price : int = 100):
+    def __init__(self, client_set : dict,  price : int = 100):
         """
         Creating ID to client, and wainting for love... waiting for love... tu-tu-tu
         :var ClientID
@@ -25,7 +13,7 @@ class Agent:
         self.critical_price = 100
         self.pref_price = price
         self.ClientID = str(random.randint(0, 100))
-        self.resources = clientSet
+        self.resources = client_set
         self.count_of_function = ( "factorial", str(random.randint(0, 5000)) )
         self.PAGENT = list()
 
@@ -33,19 +21,33 @@ class Agent:
         host = target_host
         port = target_port
         data = self.sendTo(host, port, self.generateDictOrder(type_hs)).split("|")
-        print(data)
+        print("\n<<<<<<<<<<<<<<<<<<")
+        if len(data) == 4:
+            print("port:{}\nhardware:{}%\nfunction:{}%\nprice:{}zl".format(*data))
+        elif len(data) == 2:
+            print("[GET] from\nAgent host:{}\nPort:{}".format(*data))
+        else:
+            print("[ERROR]")
         return data
 
     def accept(self):
-        vaga= 1
-        for x in range(len(self.PAGENT) - 1):
-            vaga +=0.1
-            print(self.PAGENT[x])
-            if(float(self.PAGENT[x][3]) < self.pref_price):
+        """
+        self.PAGENT - <class type(dict)>
+        self.PAGENT - [[port, hardware%, function%, price]...]
+        :return:
+        """
+        waga = 1
+        for x in range(len(self.PAGENT)):
+            finalna_waga = float(self.PAGENT[x][1]) / 1000 + waga
+            finalna_waga = finalna_waga + float(self.PAGENT[x][2]) / 1000
+            #print(self.PAGENT[x])
+            if float(self.PAGENT[x][3]) < float(self.pref_price*finalna_waga):
                 return self.PAGENT[x][0]
+            waga += 0.1
         return self.PAGENT[len(self.PAGENT)-1][0]
 
-    def generateDictOrder(self, req_type:str):
+    def generateDictOrder(self, req_type: str):
+        print(self.resources)
         return req_type+'|' + codecs.encode(pickle.dumps(self.resources), "base64").decode()
 
     def sendTo(self, host, port, requst):
@@ -57,7 +59,6 @@ class Agent:
             except ConnectionError as socketConnectionError:
                 print(socketConnectionError, " the host {} can not be reached".format(host))
                 raise IOError
-            print(requst)
             client.send(bytearray(requst, encoding="unicode-escape"))
             responce = client.recv(2048)
             return str(responce, encoding="unicode-escape")
--- static/classes/Pipeline/Agents/PipeAgent.py
+++ static/classes/Pipeline/Agents/PipeAgent.py
@@ -1,5 +1,6 @@
 from static.classes.Pipeline.PipeBuilder import PipeBuilder
 from static.classes.Pipeline.Controller import ControllerServer
+
 import socket
 import threading
 import pickle
@@ -11,9 +12,10 @@ class Agent:
 
     def __init__(self, dictionary, host: str="0.0.0.0", port: int=9999):
 
-        self.manager = PipeBuilder(9999, "/home/serhii/Projects/llapCloudFlask/static/tool/Binary/cpuController")
-        self.CPUserver = ControllerServer("/home/serhii/Projects/llapCloudFlask/static/tool/Binary/cpuController")
+        self.manager = PipeBuilder(port, "/home/serhii/Projects/llapCloudFlask/static/tool/Binary/cpuController")
+        self.CPUserver = ControllerServer("/home/serhii/Projects/llapCloudFlask/static/tool/Binary/cpuController" )
         self.CPUserver.start()
+        self.Lock = threading.RLock
 
         self.AgentDictionary = dictionary
         # ip and port to listening
@@ -23,31 +25,33 @@ class Agent:
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.bind((self.bind_ip, self.bind_port))
         try:
-            self.server.listen(5)
+            self.server.listen(6)
         except:
             self.server.close()
 
     def __handle_client(self, client_socket: socket):
         request = client_socket.recv(1024)
-        # print("[#] Odebrano : ", str(request, encoding="unicode-escape"))
         selfproto, data = str(request, encoding="unicode-escape").split("|")
-        print(data)
+        #print(data)
         if(selfproto == "order"):
             data = pickle.loads(codecs.decode(data.encode(), "base64"))
-            responce = bytearray(self.compatible(data)+self.generate_price(data["fun"]),encoding="unicode-escape")
+            responce = bytearray(self.compatible(data)+self.generate_price(), encoding="unicode-escape")
         elif(selfproto == "accept"):
             data = pickle.loads(codecs.decode(data.encode(), "base64"))
             responce = bytearray("{}|{}".format(self.bind_ip, self.bind_port), encoding="unicode-escape")
             ### test function value
-            self.manager.addProcess("factorial", 5)
-            self.manager.addProcess("bublesort", [1, 2, 3, 4, 5])
-            self.manager.addProcess("searching", ["a", "d", "f"], "dasf")
-            self.manager.buildQueue()
-            print(" RUN FOREST RUN!!!!")
+            for _ in range(data["function"]["factorial"]):
+                self.manager.addProcess("factorial", 5)
+            for _ in range(data["function"]["bublesort"]):
+                self.manager.addProcess("bublesort", list(range(random.randint(1,10))))
+            for _ in range(data["function"]["searching"]):
+                self.manager.addProcess("searching", [random.choice("qwertyusdfghjklxcvbnm") for _ in range(random.randint(5, 10))], random.choice("qwertyusdfghjklxcvbnm"))
+            #self.manager.addProcess("bublesort", list(range(random.randint(1,10))))
+            #self.manager.addProcess("searching", ["a", "d", "f"], "dasf")
+            self.manager.buildQueue(mechanic_of_cpu=self.CPUserver.get_cpu, cpu_percent=self.generate_critical_cpu(data=data))
             self.run()
         else:
             responce = bytearray("[!] EMPTY REQUEST")
-        print("[^] Wysylania packeta z powrotem")
         client_socket.send(responce)
         client_socket.close()
 
@@ -75,12 +79,16 @@ class Agent:
         searching = self.AgentDictionary["function"]["searching"] / dictionary["function"]["searching"]*100
         machine = ((x(cpu) + x(disk) + x(ram)) / 3)
         function = (x(factorial) + x(bublesort) + x(searching)) / 3
+        self.Machine_price=((self.AgentDictionary["machine"]["cpu"] + self.AgentDictionary["machine"]["disk"] + self.AgentDictionary["machine"]["ram"]) / 3)
+        self.User_price = ((dictionary["function"]["factorial"]+ dictionary["function"]["bublesort"]+ dictionary["function"]["searching"] / 3))
         return "{}|{:.2f}|{:.2f}|".format(self.bind_port, machine,  function)
 
-    def generate_price(self, func):
-        price = random.randint(6, 20) * self.AgentDictionary["function"][func] / 10
-
+    def generate_price(self):
+        price = random.randint(1, 11) / 10 * ((self.Machine_price + self.User_price) / 2)
         return "{:.2f}".format(price)
+
+    def generate_critical_cpu(self, data):
+        return lambda: int(data["machine"]["cpu"])
 
     def listening(self):
         print("[INFO] address [ {}:{} ] Start-up Agent-server ".format(self.bind_ip, self.bind_port))
@@ -92,25 +100,33 @@ class Agent:
                 client_handler.start()
             except KeyboardInterrupt:
                 print("[!] Server keyboard interrupt, stoping listening")
-                self.__del__()
+                self.CPUserver.TThread.do_run = False
+                self.manager.deleteTable()
+                self.CPUserver.join()
+                self.server.close()
+                del (self.manager)
+                del (self.CPUserver)
+                del (self.server)
+                del (self.Lock)
+                break
+            except RuntimeError as re:
+                self.CPUserver.TThread.do_run = False
+                self.manager.deleteTable()
+                self.CPUserver.join()
+                self.server.close()
+                del (self.manager)
+                del (self.CPUserver)
+                del (self.server)
                 break
 
     # ------------{PIPE-MANAGER}-------------#
 
     def run(self) -> int:
         if self.CPUserver.CPU > 0 and self.CPUserver.CPU < 90:
-            for i in range(self.manager.queue.__len__()):
+            for _ in range(0,  self.manager.queue.__len__()):
                 try:
-
-                    self.manager.queue[i].start()
-                    del (self.manager.queue[i])
+                    container = self.manager.queue.pop()
+                    container.start()
                 except Exception as e:
-                    print(e)
                     break
         return 0
-
-    def __del__(self):
-
-        self.CPUserver.TThread.do_run = False
-        self.manager.deleteTable()
-        self.CPUserver.join()
